c0790fa8f62e6fb6d5da6555260569285485b2fc
mamemaru00 <k.shouhei0711@gmail.com>
2022-10-25 14:20:09 +0900

    create

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\TodoCreate;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/todos/create", TodoCreate::class)->name("todos.create");
